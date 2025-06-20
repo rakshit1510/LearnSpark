ee36261b4b683a228fa9aeb7937aa125ff419ca2
rakshit1510 <gargrakshit10@gmail.com>
2025-06-20 22:33:24 +0530

    Add models for OTP, Category, Course, CourseProgress, RatingAndReview, Section, and SubSection

---
+++ server/src/utils/mailSender.js
@@ -0,0 +1,32 @@
+import nodemailer from 'nodemailer';
+
+const mailSender = async(email,title,body)=>{
+    
+    try {
+        
+        const transporter = nodemailer.createTransport({
+            host: process.env.SMTP_HOST,
+            port: process.env.SMTP_PORT,
+            secure: process.env.SMTP_SECURE === 'true',
+            auth: {
+                user: process.env.MAIL_USER,
+                pass: process.env.MAIL_PASS,
+            },
+        });
+
+        const info = await transporter.sendMail({
+            from: `"LearnSpark" <${process.env.MAIL_USER}>`, // sender address
+            to: email, // list of receivers
+            subject: title, // Subject line
+            text: body, // plain text body
+           html: `<p>${body}</p>`, // html body
+        });
+
+        return info;
+        
+    } catch (error) {
+        console.error('Error creating mail transporter:', error);
+        throw error; // Rethrow the error after logging
+    }
+}
+export default mailSender;
