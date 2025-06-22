2ff021faa81b2824bf541a8f0006c69933fbc051
rakshit1510 <gargrakshit10@gmail.com>
2025-06-23 02:42:43 +0530

    created basic app on react

--- server/src/routes/course.routes.js
+++ server/src/routes/course.routes.js
@@ -136,4 +136,88 @@ router.route('/getAverageRating')
 router.route('/getReviews')
     .get(getAllRatingReviews);
 
+
+// ********************************************************************************************************
+//                                      Course routes
+// ********************************************************************************************************
+
+router.route('/createCourse')
+    .post(verifyJWT, isInstructor, upload.single('thumbnail'), createCourse);
+
+router.route('/editCourse')
+    .post(verifyJWT, isInstructor, upload.single('thumbnail'), editCourse);
+
+router.route('/getCourseDetails')
+    .post(getCourseDetails);
+
+router.route('/getAllCourses')
+    .get(getAllCourses);
+
+router.route('/getFullCourseDetails')
+    .post(verifyJWT, getFullCourseDetails);
+
+router.route('/getInstructorCourses')
+    .get(verifyJWT, isInstructor, getInstructorCourses);
+
+router.route('/deleteCourse')
+    .delete(verifyJWT, isInstructor, deleteCourse);
+
+router.route('/updateCourseProgress')
+    .post(verifyJWT, isStudent, updateCourseProgress);
+
+// ********************************************************************************************************
+//                                      Section routes
+// ********************************************************************************************************
+
+router.route('/addSection')
+    .post(verifyJWT, isInstructor, createSection);
+
+router.route('/updateSection')
+    .post(verifyJWT, isInstructor, updateSection);
+
+router.route('/deleteSection')
+    .post(verifyJWT, isInstructor, deleteSection);
+
+// ********************************************************************************************************
+//                                      SubSection routes
+// ********************************************************************************************************
+
+router.route('/addSubSection')
+    .post(verifyJWT, isInstructor, upload.single('video'), createSubSection);
+
+router.route('/updateSubSection')
+    .post(verifyJWT, isInstructor, upload.single('video'), updateSubSection);
+
+router.route('/deleteSubSection')
+    .post(verifyJWT, isInstructor, deleteSubSection);
+
+// ********************************************************************************************************
+//                                      Category routes (Only by Admin)
+// ********************************************************************************************************
+
+router.route('/createCategory')
+    .post(verifyJWT, isAdmin, createCategory);
+
+router.route('/deleteCategory')
+    .delete(verifyJWT, isAdmin, deleteCategory);
+
+router.route('/showAllCategories')
+    .get(showAllCategories);
+
+router.route('/getCategoryPageDetails')
+    .post(getCategoryPageDetails);
+
+// ********************************************************************************************************
+//                                      Rating and Review
+// ********************************************************************************************************
+
+router.route('/createRating')
+    .post(verifyJWT, isStudent, createRating);
+
+router.route('/getAverageRating')
+    .get(getAverageRating);
+
+router.route('/getReviews')
+    .get(getAllRatingReviews);
+
 export default router;
