7e8b857911e1ab07ed263a113a62721baae56183
rakshit1510 <gargrakshit10@gmail.com>
2025-08-31 23:56:31 +0530

    chgange

--- client/src/components/core/Auth/ProfileDropDown.jsx
+++ client/src/components/core/Auth/ProfileDropDown.jsx
@@ -4,7 +4,7 @@ import { AiOutlineCaretDown } from "react-icons/ai"
 import { VscDashboard, VscSignOut } from "react-icons/vsc"
 import { useDispatch, useSelector } from "react-redux"
 import { Link, useNavigate } from "react-router-dom"
-import useOnClickOutside from "../../../hooks/useOnClickOutside.js"
+import useOnClickOutside from "../../../../src/hooks/useOnClickOutside.js"
 import { logout } from "../../../services/operations/authAPI"
 import Img from './../../common/Img';
 
