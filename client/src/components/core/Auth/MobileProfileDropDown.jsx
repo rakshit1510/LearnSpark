8095fe483684353e21c4269749c7ded555147433
rakshit1510 <gargrakshit10@gmail.com>
2025-09-01 01:20:38 +0530

    change_new

--- client/src/components/core/Auth/MobileProfileDropDown.jsx
+++ client/src/components/core/Auth/MobileProfileDropDown.jsx
@@ -3,7 +3,7 @@ import { useRef, useState, useEffect } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { Link, useNavigate } from "react-router-dom"
 
-import useOnClickOutside from "../../../hooks/useOnClickOutside"
+import useOnClickOutside from "../hooks/useOnClickOutside"
 import Img from './../../common/Img';
 
 import { logout } from "../../../services/operations/authAPI"
